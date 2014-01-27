6335ae5a5749a6f7d8267597648f453efcee9435
Abdoul Diallo <akdiallo91@gmail.com>
2014-01-27 19:04:54 +0100

    Presenter added

---
+++ src/appscool/presenter/AuthentificationAdminPresenter.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package appscool.presenter;
+
+/**
+ *
+ * @author khadre
+ */
+public class AuthentificationAdminPresenter {
+
+}
