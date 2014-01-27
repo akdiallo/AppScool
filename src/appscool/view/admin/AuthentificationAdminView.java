9de97cc8c23c685356cf4c3d27782b55c1ad5c0f
amadou <93asonko@gmail.com>
2014-01-27 18:59:26 +0100

    merge2

---
+++ src/appscool/view/admin/AuthentificationAdminView.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package appscool.view.admin;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author amma
+ */
+public class AuthentificationAdminView{
+    /*@Override
+    public void start(Stage primaryStage){
+        
+        
+        VBox box=new VBox(10);
+        Scene scene=new Scene(box,500,500,Color.BLUE);
+        
+        TextField login=new TextField("Login");
+        box.getChildren().add(login);
+        
+        PasswordField password=new PasswordField();
+        box.getChildren().add(password);
+        
+        Button bCon=new Button("Se connecter");
+        box.getChildren().add(bCon);
+        
+        Button bReturn=new Button("Retour");
+        box.getChildren().add(bReturn);
+        
+        Text authen=new Text("Authentification - Administrateur");
+        box.getChildren().add(authen);
+        
+        primaryStage.setScene(scene);
+        
+    }
+    public static void main(String[] args) {
+        launch(args);
+    }*/
+}
